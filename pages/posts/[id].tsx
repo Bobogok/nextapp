e7976882fee999a82a0bd0ca2a77c903151ba18b
Bobogok <vania.morskov@yandex.ru>
2022-04-09 00:42:28 +0300

    добавил спиннер

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,9 +1,42 @@
 import axios from "axios"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import Spinner from "styles/Spinner"
 
-function Post({ post }: { post: any }) {
-  return (
-    JSON.stringify(post)
+function Post({ post: serverPost }: { post: any }) {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      url !== router.pathname ? setLoading(true) : setLoading(false);
+    };
+    const handleComplete = (url: string) => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  }, [router]);
+
+  return ( 
+    <>
+      {loading && <Spinner />}
+      <Link href={`${serverPost.id + 1}`}>
+        <a>Сдующий пост</a>
+      </Link>
+      <hr />
+      <div />
+      {JSON.stringify(serverPost)}
+    </>
   )
+
 }
 
 // This function gets called at build time
@@ -23,7 +56,7 @@ export async function getStaticPaths() {
 }
 
 // This also gets called at build time
-export async function getStaticProps({ params }: { params: any }) {
+export async function getStaticProps({ params, req }: { params: any, req: any }) {
   // params contains the post `id`.
   // If the route is like /posts/1, then params.id is 1
   const res = await axios(`http://localhost:3001/posts/${params.id}`)
